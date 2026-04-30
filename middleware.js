c10d85632b050e8084d280ff34fc1949e5c020f4
M.M Danesh Pajouh <37251185+LordNecromancer@users.noreply.github.com>
2026-04-30 06:24:17 +0330

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(request) {
+  const url = request.nextUrl;
+
+  const target = `https://lordnecromancer.top${url.pathname}${url.search}`;
+
+  return NextResponse.rewrite(target);
+}
+
+export const config = {
+  matcher: '/:path*',
+};
