64ed0fc29e26cec762bfd1f13396fb2ea222b9c2
M.M Danesh Pajouh <37251185+LordNecromancer@users.noreply.github.com>
2026-04-30 07:13:56 +0330

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,22 +1,22 @@
-// import { NextResponse } from 'next/server';
-
-// export function middleware(request) {
-//   const url = request.nextUrl;
-
-//   const target = `https://lordnecromancer.top${url.pathname}${url.search}`;
-
-//   return NextResponse.rewrite(target);
-// }
-
-// export const config = {
-//   matcher: '/:path*',
-// };
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-  return new NextResponse("MIDDLEWARE WORKING");
+  const url = request.nextUrl;
+
+  const target = `https://lordnecromancer.top${url.pathname}${url.search}`;
+
+  return NextResponse.rewrite(target);
 }
 
 export const config = {
   matcher: '/:path*',
 };
+// import { NextResponse } from 'next/server';
+
+// export function middleware(request) {
+//   return new NextResponse("MIDDLEWARE WORKING");
+// }
+
+// export const config = {
+//   matcher: '/:path*',
+// };
